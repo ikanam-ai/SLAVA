13e29fedf55d6262491eb61ff9c337598da87482
timmSi <hello.timm@mail.ru>
2024-10-25 01:40:01 +0300

    upgrade piplines hf

--- slava/models/huggingface.py
+++ slava/models/huggingface.py
@@ -1,7 +1,5 @@
-from langchain_huggingface import HuggingFaceEndpoint
-
+from langchain_huggingface import HuggingFacePipeline
 from slava.config import HUGGINGFACE_MODEL_MAX_TOKENS, HUGGINGFACE_MODEL_TEMPERATURE, HUGGINGFACE_MODEL_TOP_K
-
 
 class HuggingFaceModel:
     def __init__(
@@ -11,10 +9,13 @@ class HuggingFaceModel:
         top_k: int = HUGGINGFACE_MODEL_TOP_K,
         temperature: float = HUGGINGFACE_MODEL_TEMPERATURE,
     ):
-        self.model = HuggingFaceEndpoint(
-            repo_id=model_name,
+        self.model = HuggingFacePipeline.from_model_id(
+            model_id=model_name,
+            device=0,
             task="text-generation",
-            max_new_tokens=max_tokens,
-            top_k=top_k,
-            temperature=temperature,
+            pipeline_kwargs={
+                "max_new_tokens": max_tokens,
+                "top_k": top_k,
+                "temperature": temperature
+            }
         )
